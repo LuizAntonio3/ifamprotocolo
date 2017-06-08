aa8f0e9d81b145595cff4dc0f6bce92066a86667
RobsonRojas <robson.rojas@gmail.com>
2017-06-07 21:15:14 -0400

    first login page working

--- source-code/public/js/index.js
+++ source-code/public/js/index.js
@@ -1,51 +1,55 @@
-const url = "https://localhost:3000";
+const url = "http://localhost:3000";
 
 // Início jQuery
-$(document).on('ready', function(e) {
+$(document).ready(function(e) {
 
 // evento click no botão login (quando eu clicar no botão login ele vai executar esses comandos)
-$("#btnSalvar").click(function(e){
+$("#btnLogin").click(function(e){
 	enviarForm();
 	e.preventDefault();
 });
 
 function enviarForm(){
-		var vLogin = $("#email").val(); // Estou recebendo o valor do campo Login
-		var vSenha = $("#senha").val(); // Estou recebendo o valor do campo Senha
+		var vLogin = $("#inputEmail").val(); // Estou recebendo o valor do campo Login
+		var vSenha = $("#inputPassword").val(); // Estou recebendo o valor do campo Senha
 		// alert("login123: "+vLogin);
 
-		// if (vLogin.length == 0) { // Aqui eu verifico se digitou algo no login
-		// 	myFuncitionAlert("O campo Login é Obrigatório.");
-		// 	$("#cmpLogin").focus(); // Faço o foco voltar para o campo de login
-		// } else if (vSenha.length == 0) { // Aqui eu verifico se digitou algo no login
-		// 	myFuncitionAlert("O campo Senha é Obrigatório.");
-		// 	$("#cmpSenha").focus();// Faço o foco voltar para o campo de senha
-		// } else { // Caso o login e senha estiverem preenchidos, entra aqui.
+		if (vLogin.length == 0) { // Aqui eu verifico se digitou algo no login
+			alert("O campo Email é Obrigatório.");
+		} else if (vSenha.length == 0) { // Aqui eu verifico se digitou algo no login
+			alert("O campo Senha é Obrigatório.");
+		}
 			
-			// Aqui vou usar o ajax (jquery), onde ele vai verificar em outro arquivo se o usuário e senha existe
-			$.post(url + "/api/v1/usuario/login", {"email": vLogin, "senha": vSenha}, function(retorno){
-
-				if(retorno.mensagem) { // caso o retorno for TRUE
-
-					try{
-			  		var obj = JSON.parse(retorno.res);
-
-            // sucesso
-            if(0)
-            {
-
-            }
+		// Aqui vou usar o ajax (jquery), onde ele vai verificar em outro arquivo se o usuário e senha existe
+		$.post(url + "/api/v1/usuario/login", 
+				{
+					"email": vLogin, 
+					"senha": vSenha
+				},
+				function(retorno){
 					
-            window.location=urlServidor+"/cadastrar_protocolo.html";
-            
-					}catch(e){
+					console.log(retorno)
 
-						//window.location=urlServidor+"/cadastrar_protocolo.html";
+					if (retorno) {
+						try {
+							var obj = JSON.parse(retorno.resp);
+
+							if (obj) {
+								alert("ULogin Realiza do com   sucesso.");
+
+								$("#email").val("");
+								$("#senha").val("");
+
+								//window.location=urlServidor+"/cadastrar_protocolo.html";
+							} else {
+								alert("UEmail ou senha ninválidos");
+							}
+						} catch (error) {
+							alert("Resposta inválida do servidor.");
+						}
+					}else{
+						alert("Resposta inválida do servidor.");
 					}
-				 } 
-				 else { // caso o retorno for FALSE
-
-				}
 			  }, 'json');
 		}
 });
