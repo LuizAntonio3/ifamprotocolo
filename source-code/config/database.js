5620f510decde35955e131e010003c9024629bb8
RobsonRojas <robson.rojas@gmail.com>
2017-06-10 10:51:47 -0400

    Updating files

--- source-code/config/database.js
+++ source-code/config/database.js
@@ -1,12 +1,5 @@
 var knex = require('knex')({
   client: 'mysql',
-  // connection: {
-  //   host: 'ec2-184-72-246-219.compute-1.amazonaws.com',
-  //   user: 'sslvislnyikbdn',
-  //   password: '003ed630f73700e8d8be5f85cd8471eb3077301edba34ea63f6ad593cd865a7a',
-  //   database: 'd7t8e1cmrvf86j',
-  //   ssl: true
-  // }
   connection: {
     host: 'localhost',
     user: 'root',
