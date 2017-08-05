6aa6da6db61fb33ec3ef793811c6db5dfd5fe57c
RobsonRojas <robson.rojas@gmail.com>
2017-08-04 23:25:24 -0400

    Implementing componente requisicoes

--- reactjsapp/src/home.js
+++ reactjsapp/src/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SideBarMenu from './sidebarmenu.js'
+import Requisicoes from './requisicoes.js'
 import './home.css';
 
 const homeScreens = [
@@ -28,16 +29,12 @@ class Home extends Component {
         }
 
         this.handleSideBarItemClick = this.handleSideBarItemClick.bind(this);
-        this.handleUserMenuItemClick = this.handleUserMenuItemClick.bind(this);
     }
 
     handleSideBarItemClick(){
         this.setState({
             current_screen: 1
         })
-    }
-    handleUserMenuItemClick(){
-
     }
 
     render() {
@@ -48,18 +45,7 @@ class Home extends Component {
                     onItemClick={this.handleSideBarItemClick} 
                     options={homeScreens} 
                     userName={this.state.user_name}/>
-                <div id="page-content-wrapper">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <h1>Simple Sidebar</h1>
-                                <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                                <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Requisicoes />
             </div>
         </div>
         );
