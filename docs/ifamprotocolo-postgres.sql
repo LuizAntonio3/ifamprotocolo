5fb2511b64af7bbe0f58588dfaf3521c58c21e12
RobsonRojas <robson.rojas@gmail.com>
2017-08-06 18:53:21 -0400

    Arquivo sql para mysql

---
+++ docs/ifamprotocolo-postgres.sql
@@ -0,0 +1,105 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 27-Maio-2017 às 23:16
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: ifamprotocolo
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela anexos
+--
+
+CREATE TABLE IF NOT EXISTS anexos (
+  id INTEGER(11) NOT NULL,
+  caminho varchar(256) NOT NULL,
+  idSolicitacao int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela departamento
+--
+
+CREATE TABLE IF NOT EXISTS departamento (
+  id int(11) NOT NULL,
+  Nome varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela servicodepart
+--
+
+CREATE TABLE IF NOT EXISTS servicodepart (
+  id int(11) NOT NULL,
+  idServico int(11) NOT NULL,
+  idDepartamento int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela servicos
+--
+
+CREATE TABLE IF NOT EXISTS servicos (
+  id int(11) NOT NULL,
+  Nome varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela solicitação
+--
+
+CREATE TABLE IF NOT EXISTS solicitação (
+  id varchar(11) NOT NULL,
+  Data date NOT NULL,
+  Status varchar(20) NOT NULL,
+  idServico int(11) NOT NULL,
+  idUsuario int(11) NOT NULL,
+  idDepartamento int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela usuario
+--
+
+CREATE TABLE IF NOT EXISTS usuario (
+  nome varchar(20) DEFAULT NULL,
+  senha varchar(20) DEFAULT NULL,
+  email varchar(254) NOT NULL,
+  id int(11) NOT NULL,
+  telefone int(11) NOT NULL,
+  matricula varchar(13) NOT NULL,
+  logradouro varchar(150) NOT NULL,
+  numero varchar(5) NOT NULL,
+  bairro varchar(25) NOT NULL,
+  complemento varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
