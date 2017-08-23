5a44b668f3bafd07576fcf34429ac2b23c29b7b6
RobsonRojas <robson.rojas@gmail.com>
2017-08-22 21:30:54 -0400

    Removendo dependencia do login para abrir, atualizando formulario de requisicao

--- reactjsapp/src/js/models/usuario.js
+++ reactjsapp/src/js/models/usuario.js
@@ -36,25 +36,25 @@ const Usuario = {
 	create: function (usuario, next) {
 		if (usuario) {
 			if(usuario.nome && usuario.nome.length === 0) 
-				return {success: false, msg: "O campo Nome é obrigatório."};
+				next({success: false, msg: "O campo Nome é obrigatório."});
 			if(usuario.tipo && usuario.tipo.length === 0) 
-				return {success: false, msg: "O campo Tipo é obrigatório."};
+				next({success: false, msg: "O campo Tipo é obrigatório."});
 			if(usuario.email && usuario.email.length === 0) 
-				return {success: false, msg: "O campo Email é obrigatório."};
+				next({success: false, msg: "O campo Email é obrigatório."});
 			if(usuario.senha && usuario.senha.length === 0) 
-				return {success: false, msg: "O campo Senha é obrigatório."};
+				next({success: false, msg: "O campo Senha é obrigatório."});
 			if(usuario.telefone && usuario.telefone.length === 0) 
-				return {success: false, msg: "O campo Telefone é obrigatório."};
+				next({success: false, msg: "O campo Telefone é obrigatório."});
 			if(usuario.matricula && usuario.matricula.length === 0) 
-				return {success: false, msg: "O campo Matricula é obrigatório."};
+				next({success: false, msg: "O campo Matricula é obrigatório."});
 			if(usuario.logradouro && usuario.logradouro.length === 0) 
-				return {success: false, msg: "O campo Logradouro é obrigatório."};
+				next({success: false, msg: "O campo Logradouro é obrigatório."});
 			if(usuario.numero && usuario.numero.length === 0) 
-				return {success: false, msg: "O campo Numero é obrigatório."};
+				next({success: false, msg: "O campo Numero é obrigatório."});
 			if(usuario.bairro && usuario.bairro.length === 0) 
-				return {success: false, msg: "O campo Bairro é obrigatório."};
+				next({success: false, msg: "O campo Bairro é obrigatório."});
 			if(usuario.complemento && usuario.complemento.length === 0) 
-				return {success: false, msg: "O campo Complemento é obrigatório."};
+				next({success: false, msg: "O campo Complemento é obrigatório."});
 
 			// Salvando
 			request
@@ -92,12 +92,16 @@ const Usuario = {
 	},
 	login: function(vLogin, vSenha, next){
 
+		next({success: true, msg: "Login Realizado com sucesso.", data: null});	
+
 		console.log("aqui");
 
 		if (vLogin.length === 0) { // Aqui eu verifico se digitou algo no login
-			return {success: false, msg: "O campo Email é Obrigatório."};
+			next({success: false, msg: "O campo Email é Obrigatório."});
+			return;
 		} else if (vSenha.length === 0) { // Aqui eu verifico se digitou algo no login
-			return {success: false, msg: "O campo Senha é Obrigatório."};
+			next({success: false, msg: "O campo Senha é Obrigatório."});
+			return;
 		}
 
 		request
--- reactjsapp/src/requisicaoform.js
+++ reactjsapp/src/requisicaoform.js
@@ -27,44 +27,50 @@ class RequisicaoForm extends Component {
       <div>
         <Form onSaved={this.handleSubmit} onCancel={this.handleBtnCancelClicked}>
             <div className="form-group">
-            <label htmlFor="tipo" className="col-sm-3 control-label">
-                    <b >Incluir solicitante</b>
+            <label>
+                    <b >Incluir solicitante:</b>
             </label>
             </div>
             <div className="form-group">
-                <label htmlFor="tipo" className="col-sm-3 control-label">
+                <label htmlFor="tipo" className="col-sm-2 control-label">
                     Nome
                 </label>
-                <div className="col-sm-9">
+                <div className="col-sm-4">
                     <input type="text" 
                             id="nome" 
                             placeholder="Nome" 
                             className="form-control"/>
                 </div>
-                <i className="glyphicon glyphicon-search col-sm-8">
-                </i>
+                <div className="col-sm-3">
+                    <i className="glyphicon glyphicon-search">
+                    </i>
+                </div>
+                
             </div>
             <div className="form-group">
-                <label htmlFor="tipo" className="col-sm-3 control-label">
+                <label htmlFor="tipo" className="col-sm-2 control-label">
                     Matrícula
                 </label>
-                <div className="col-sm-9">
+                <div className="col-sm-4">
                     <input type="text" 
                             id="matricula" 
                             placeholder="123" 
                             className="form-control"/>
                 </div>
-                <i className="glyphicon glyphicon-search col-sm-8">
-                </i>
+                <div className="col-sm-3">
+                    <i className="glyphicon glyphicon-search">
+                    </i>
+                </div>
             </div>
 			<div>
-				<div className="col-sm-5"><hr /></div>
-				<div className="col-sm-2"><b>Dados do solicitante</b></div>
-				<div className="col-sm-5"><hr /></div>
-
                 <div className="form-group">
-                    <label htmlFor="nome" className="col-sm-3 control-label">
-                        Nome
+                    <label>
+                        <b >Dados do solicitante:</b>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="nome" className="col-sm-2 control-label">
+                        Nome:
                     </label>
                     <div className="col-sm-9">
                         <label htmlFor="nome" className="col-sm-3 control-label">
@@ -73,8 +79,8 @@ class RequisicaoForm extends Component {
                     </div>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="matricula" className="col-sm-3 control-label">
-                        Matrícula
+                    <label htmlFor="matricula" className="col-sm-2 control-label">
+                        Matrícula:
                     </label>
                     <div className="col-sm-9">
                     <label htmlFor="matricula" className="col-sm-3 control-label">
@@ -84,152 +90,86 @@ class RequisicaoForm extends Component {
                 </div>                
 			</div>
 			<div>
-				<div className="col-sm-5"><hr/></div>
-				<div className="col-sm-2"><b>Serviços</b></div>
-				<div className="col-sm-5"><hr/></div>
-
-
-            <div>
-                <li>
-                    <ul>
-                <label>
-                <input for="laboratorio" type="checkbox" value="0"/>
-                    Laboratorio
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="saladeaula" type="checkbox" value="0"/>
-                    Sala de aula
-                </label>                        
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="quadrapoliesportiva" type="checkbox" value="0"/>
-                    Quadra poliesportiva
-                </label>                        
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="historicoescolar" type="checkbox" value="0"/>
-                    Histórico Escolar
-                </label>                        
-                    </ul>
-                    <ul>
-                <label>
-                <input for="certificadoensinomedio" type="checkbox" value="0"/>
-                    Certificado do Ensino Médio
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="diplomadeniveltecnico" type="checkbox" value="0"/>
-                    Diploma de Nível Técnico
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="transferenciaescolar" type="checkbox" value="0"/>
-                    Transferência Escolar
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="justificativafaltas" type="checkbox" value="0"/>
-                    Justificativa de Faltas
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="retornoaescola" type="checkbox" value="0"/>
-                    Retorno á escola
-                </label>
-
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="declaracaoescolaridade" type="checkbox" value="0"/>
-                    Declaração de escolaridade
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="trancamentomatricula" type="checkbox" value="0"/>
-                    Trancamento de Matrícula
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="cancelamentomatricula" type="checkbox" value="0"/>
-                    Cancelamento de Matrícula
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="correcaonotas" type="checkbox" value="0"/>
-                    Correção de Notas
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="avaliacao2achamada" type="checkbox" value="0"/>
-                    Avaliação de 2a Chamada
-                </label>
-                    </ul>
-                    <ul>
-
-                <label>
-                <input for="outros" type="checkbox" value="0"/>
-                    Outros
-                </label>
-                    </ul>
-                    <ul>
-
-                    </ul>
-                    <ul>
-
-                    </ul>
-                    <ul>
-
-                    </ul>
-                </li>
-
-
-                    </div>
-                </div>
-			<div>
-				<div className="col-sm-5"><hr/></div>
-				<div className="col-sm-2"><b>Anexos</b></div>
-				<div className="col-sm-5"><hr/></div>
-
                 <div className="form-group">
-                    <label className="control-label col-sm-5" >Anexo 1: </label>
-                    <div className="col-sm-2">
-                        <input type="text" id="inputAnexo1" name="anexo_1"/>
+                    <label>
+                        <b >Serviços:</b>
+                    </label>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <label >
+                                <input htmlFor="laboratorio" type="checkbox" value="0"/> 
+                                Laboratorio
+                            </label>                        
+                        </li>
+                        <li>
+                            <label>
+                                <input htmlFor="saladeaula" type="checkbox" value="0"/>
+                                    Sala de aula
+                            </label>                        
+                        </li>
+                        <li>
+                            <label>
+                                <input htmlFor="quadrapoliesportiva" type="checkbox" value="0"/>
+                                    Quadra poliesportiva
+                            </label>                        
+                        </li>
+                    </ul>
+                </div>
+            </div>
+			<div>
+                <div className="form-group">
+                    <label>
+                        <b >Anexos:</b>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label className="control-label col-sm-2" >Anexo 1: </label>
+                    <div className="col-sm-3">
+                        <input type="text" 
+                                id="inputAnexo1" 
+                                name="anexo_1"
+                                className="form-control"/>
                     </div>
-                    <i className="glyphicon glyphicon-paperclip col-sm-5"></i>
+                    <div className="col-sm-1">
+                        <i className="glyphicon glyphicon-paperclip">
+                        </i>
+                    </div>
+                    <div className="col-sm-1">
+                        <i className="glyphicon glyphicon-plus"></i>
+                    </div>
                 </div>
 			</div> 
     		<div>
-				<div className="col-sm-5"><hr/></div>
-				<div className="col-sm-2"><b>Setores</b></div>
-				<div className="col-sm-5"><hr/></div>
-
-
-			</div>
-                
+                <div className="form-group">
+                    <label>
+                        <b>Departamentos:</b>
+                    </label>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <label >
+                                <input htmlFor="laboratorio" type="checkbox" value="0"/> 
+                                Secretaria
+                            </label>                        
+                        </li>
+                        <li>
+                            <label>
+                                <input htmlFor="saladeaula" type="checkbox" value="0"/>
+                                    Sala de professores
+                            </label>                        
+                        </li>
+                        <li>
+                            <label>
+                                <input htmlFor="quadrapoliesportiva" type="checkbox" value="0"/>
+                                    Diretoria
+                            </label>                        
+                        </li>
+                    </ul>
+                </div>
+			</div> 
         </Form>
       </div>
     );
