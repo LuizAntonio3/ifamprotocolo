7b2b047eebfe920f7a8e43c30d20d7c9c61e7e0a
RobsonRojas <robson.rojas@gmail.com>
2017-09-08 21:29:50 -0400

    Implementação de tela de edição de requisição

--- reactjsapp/src/requisicaoform.js
+++ reactjsapp/src/requisicaoform.js
@@ -9,12 +9,25 @@ import _anexo from './js/models/anexo.js'
 class RequisicaoForm extends Component {
     constructor(props){
         super(props);
+
+        console.log('item',this.props.item);
+
+        var id_usuario = -1;
+        var id_requisicao = -1;
+        if (this.props.item) {
+            id_usuario = this.props.item.id_usuario;
+            id_requisicao = this.props.item.id;
+        }
+
         this.state = {
-            IdSolicitante: -1,
+            id: id_requisicao,
+            IdSolicitante: id_usuario,
             listAnexos: [],
             listSolicitantes:[],
             listServicos:[],
-            listDepartamentos:[]
+            selectedServicos:[],
+            listDepartamentos:[],
+            selectedDepartamentos:[]
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -30,6 +43,47 @@ class RequisicaoForm extends Component {
         this.handleFetchDepartamentosResponse = this.handleFetchDepartamentosResponse.bind(this);
         this.handleCreateRequisicaoResponse = this.handleCreateRequisicaoResponse.bind(this);
         this.handleUploadAnexoResponse = this.handleUploadAnexoResponse.bind(this);
+
+        this.handleGetDepartamentosRequisicaoResponse = this.handleGetDepartamentosRequisicaoResponse.bind(this);
+        this.handleGetServicosRequisicaoResponse = this.handleGetServicosRequisicaoResponse.bind(this);
+        this.handleGetAnexosRequisicaoResponse = this.handleGetAnexosRequisicaoResponse.bind(this);
+
+    }
+    handleGetDepartamentosRequisicaoResponse (res) {
+        console.log("resultado get departamentos solicitacao",res);
+        if (res.success) {
+            var selectedDeps = [];
+            for (var index = 0; index < res.data.data.length; index++) {
+                selectedDeps.push(res.data.data[index].id_departamento);
+            }
+
+            console.log("new selectedDepartamentos",selectedDeps);
+            this.setState({selectedDepartamentos: selectedDeps})
+        }
+    }
+    handleGetServicosRequisicaoResponse (res) {
+        console.log("resultado get Servicos solicitacao",res);
+        if (res.success) {
+            var selected = [];
+            for (var index = 0; index < res.data.data.length; index++) {
+                selected.push(res.data.data[index].id_servico);
+            }
+
+            console.log("new selectedServicos",selected);
+            this.setState({selectedServicos: selected})
+        }
+    }
+    handleGetAnexosRequisicaoResponse (res) {
+        console.log("resultado get Anexos solicitacao",res);
+        if (res.success) {
+            var anexos = [];
+            for (var index = 0; index < res.data.data.length; index++) {
+                anexos.push(res.data.data[index]);
+            }
+
+            console.log("new listAnexos",anexos);
+            this.setState({listAnexos: anexos})
+        }
     }
     handleFetchDepartamentosResponse (res) {
         console.log(res);
@@ -37,6 +91,7 @@ class RequisicaoForm extends Component {
             const Departamentos = this.state.listDepartamentos.concat(res.data);
 
             console.log('Departamentos', this.state.listDepartamentos);
+            
             this.setState({listDepartamentos: Departamentos});
         }
     }
@@ -63,10 +118,17 @@ class RequisicaoForm extends Component {
         _usuario.listAll(this.handleFetchSolicitantesResponse)
         _servico.listAll(this.handleFetchServicosResponse);
         _departamento.listAll(this.handleFetchDepartamentosResponse);
-    }
-    componentWillMount = () => {
-        this.selectedServicos = [];
-        this.selectedDepartamentos = [];
+
+        if (this.state.id > 0) {
+            // find selected departamentos by requisicao id
+            _requisicao.getDepartamentos(this.state.id, this.handleGetDepartamentosRequisicaoResponse);
+
+            // find selected services by requisicao id
+            _requisicao.getServicos(this.state.id, this.handleGetServicosRequisicaoResponse);
+
+            // find anexos
+            _requisicao.getAnexos(this.state.id, this.handleGetAnexosRequisicaoResponse);
+        }
     }
     handleCreateRequisicaoResponse (res) {
         console.log('resposta: ' + res);
@@ -78,12 +140,11 @@ class RequisicaoForm extends Component {
             alert('Não foi possível realizar a requisição');
         }
     }
-
     handleSubmit(event){
         var data = {
             id_usuario: this.state.IdSolicitante,
-            servicos: this.selectedServicos,
-            departamentos: this.selectedDepartamentos,
+            servicos: this.state.selectedServicos,
+            departamentos: this.state.selectedDepartamentos,
             anexos: this.state.listAnexos
         };
 
@@ -100,30 +161,35 @@ class RequisicaoForm extends Component {
     handleServicoCheckBoxChange(event){
         console.log(event.target);
 
-        var index = this.selectedServicos.indexOf(event.target.id);
+        var selected = this.state.selectedServicos;
+        var index = selected.indexOf(event.target.id);
 
         if (index !== -1) {
-            this.selectedServicos.splice(index, 1);
+            selected.splice(index, 1);
         }
         else{
-            this.selectedServicos.push(event.target.id);
+            selected.push(event.target.id);
         }
 
-        console.log(this.selectedServicos);
+        console.log(selected);
+        this.setState({selectedServicos: selected})
     }
     handleDepartamentoCheckBoxChange(event){
         console.log(event.target);
 
-        var index = this.selectedDepartamentos.indexOf(event.target.id);
+        var selectedDeps = this.state.selectedDepartamentos;
+
+        var index = selectedDeps.indexOf(event.target.id);
 
         if (index !== -1) {
-            this.selectedDepartamentos.splice(index, 1);
+            selectedDeps.splice(index, 1);
         }
         else{
-            this.selectedDepartamentos.push(event.target.id);
+            selectedDeps.push(event.target.id);
         }
-        
-        console.log(this.selectedDepartamentos);
+        console.log(selectedDeps);
+
+        this.setState({selectedDepartamentos: selectedDeps})
     }
 
     handleUploadAnexoResponse (res) {
@@ -155,10 +221,13 @@ class RequisicaoForm extends Component {
         console.log('files',this.state.listAnexos);
     }
   render() {
+
     const documents = this.state.listAnexos.map((anexo, index) => {
       return <tr key={index}>
                 <td>{anexo.originalname}</td> 
-                <td><i id={index} className="glyphicon glyphicon-trash" onClick={this.handleAnexoDeleteClick}></i></td> 
+                <td><i id={index} 
+                        className="glyphicon glyphicon-trash" 
+                        onClick={this.handleAnexoDeleteClick}></i></td> 
             </tr>
     });
 
@@ -171,19 +240,58 @@ class RequisicaoForm extends Component {
                     });
 
     const servicosRender = this.state.listServicos.map((servico, idx)=>{
-    return <li key={idx}>
-        <label>
-            <input id={servico.id} type="checkbox" onClick={this.handleServicoCheckBoxChange}/> {servico.nome}
-        </label>
-    </li>
+
+        var selected = this.state.selectedServicos.find(function (item) {
+            return item == servico.id;
+        })
+
+        var checkbox;
+
+        if (selected) {
+            checkbox = <input checked 
+                            id={servico.id} 
+                            type="checkbox" 
+                            onClick={this.handleServicoCheckBoxChange}/> 
+        } else {
+            checkbox = <input 
+                            id={servico.id} 
+                            type="checkbox" 
+                            onClick={this.handleServicoCheckBoxChange}/> 
+        }
+
+        return <li key={idx}>
+            <label>
+                {checkbox}
+                {servico.nome}
+            </label>
+        </li>
     })
 
     const departamentosRender = this.state.listDepartamentos.map((departamento, idx)=>{
-    return <li key={idx}>
-        <label>
-            <input id={departamento.id} type="checkbox" onChange={this.handleDepartamentoCheckBoxChange}/> {departamento.nome}
-        </label>
-    </li>
+
+        var selectedDep = this.state.selectedDepartamentos.find(function (item) {
+            return item == departamento.id;
+        })
+
+        var checkbox;
+
+        if (selectedDep) {
+            checkbox = <input checked 
+                            id={departamento.id} 
+                            type="checkbox" 
+                            onChange={this.handleDepartamentoCheckBoxChange}/>
+        } else {
+            checkbox = <input 
+                            id={departamento.id} 
+                            type="checkbox" 
+                            onChange={this.handleDepartamentoCheckBoxChange}/>
+        }
+        return <li key={idx}>
+            <label>
+                {checkbox}
+                {departamento.nome}
+            </label>
+        </li>
     })
 
     return (
